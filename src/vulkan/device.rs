a6d986c618689e9f58332ae8284d7943faf9dc94
odanek <ondrej.danek@gmail.com>
2021-01-06 19:39:35 +0100

    Save graphics queue handle

--- src/vulkan/device.rs
+++ src/vulkan/device.rs
@@ -6,16 +6,17 @@ use super::physical_device::VkPhysicalDevice;
 
 pub struct VkDevice {
     pub handle: ash::Device,
+    pub graphics_queue: vk::Queue,
 }
 
 impl VkDevice {
     pub fn new(instance: &ash::Instance, physical_device: &VkPhysicalDevice) -> VkDevice {
-        let graphics_queue = select_graphics_queue(physical_device);        
+        let graphics_queue_family = select_graphics_queue_family(physical_device);        
         let queue_priorities = [1.0f32];
-        log::info!("Choosing graphics queue family: {}", graphics_queue);
+        log::info!("Choosing graphics queue family: {}", graphics_queue_family);
 
         let queue_create_info = vk::DeviceQueueCreateInfo::builder()
-            .queue_family_index(graphics_queue)
+            .queue_family_index(graphics_queue_family)
             .queue_priorities(&queue_priorities)
             .build();
         let queue_infos = [queue_create_info];
@@ -25,9 +26,14 @@ impl VkDevice {
             instance
                 .create_device(physical_device.handle, &device_create_info, None)
                 .expect("Unable to create logical device")
-        };        
+        };
+        
+        let graphics_queue = unsafe { handle.get_device_queue(graphics_queue_family, 0) };
 
-        VkDevice { handle }
+        VkDevice { 
+            handle,
+            graphics_queue
+        }
     }
 }
 
@@ -48,7 +54,7 @@ impl Deref for VkDevice {
     }
 }
 
-fn select_graphics_queue(physical_device: &VkPhysicalDevice) -> u32 {
+fn select_graphics_queue_family(physical_device: &VkPhysicalDevice) -> u32 {
     physical_device
         .queue_families
         .iter()
