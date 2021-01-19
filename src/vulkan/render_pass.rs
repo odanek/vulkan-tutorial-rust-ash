67f2da8ded4ff96aedead3787c0f00e7586afb51
odanek <ondrej.danek@gmail.com>
2021-01-19 19:59:46 +0100

    Fix image format warning

--- src/vulkan/render_pass.rs
+++ src/vulkan/render_pass.rs
@@ -18,7 +18,7 @@ impl VkRenderPass {
             .stencil_load_op(vk::AttachmentLoadOp::DONT_CARE)
             .stencil_store_op(vk::AttachmentStoreOp::DONT_CARE)
             .initial_layout(vk::ImageLayout::UNDEFINED)
-            .final_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+            .final_layout(vk::ImageLayout::PRESENT_SRC_KHR)
             .build();
 
         let attachment_descs = [color_attachment_desc];
